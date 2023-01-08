f30e37c3300ac498907088f9d0ff7646f76b16e1
Henry Hartzler <hartzler.henry@gmail.com>
2023-01-08 15:28:09 -0700

    fetch method for words

--- src/features/words/wordsSlice.js
+++ src/features/words/wordsSlice.js
@@ -2,94 +2,43 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { baseUrl } from '../../shared/baseUrl'
 import axios from 'axios'
 
-export const fetchSends = createAsyncThunk('sends/fetchSends', async () => {
-	const response = await axios.get(baseUrl + 'sends')
+export const fetchWords = createAsyncThunk('words/fetchWords', async (word) => {
+	const response = await axios.get(
+		`https://api.datamuse.com/words?ml=${word}&rel_trg=${word}`
+	)
 	return response.data
 })
 
-export const postSend = createAsyncThunk('sends/postSend', async (send) => {
-	try {
-		const response = await axios.post(baseUrl + 'sends', send)
-		return response.data
-	} catch (err) {
-		console.error(err)
-	}
-})
-
-export const deleteSend = createAsyncThunk(
-	'sends/deleteSend',
-	async (sendClimb) => {
-		try {
-			console.log('SEND CLIMB', sendClimb)
-			const response = await axios.delete(
-				baseUrl + 'sends/' + `${sendClimb.climbId}`,
-				sendClimb
-			)
-			return response.data
-		} catch (err) {
-			console.error(err)
-		}
-	}
-)
-
-const sendsSlice = createSlice({
-	name: 'sends',
-	initialState: { isLoading: true, errMess: null, sendsArray: [] },
+const wordsSlice = createSlice({
+	name: 'words',
+	initialState: { isLoading: true, errMess: null, wordsArray: [] },
 	reducers: {
-		logSend: (state, action) => {
-			state.sendsArray.push(action.payload)
+		logword: (state, action) => {
+			state.wordsArray.push(action.payload)
 		},
-		removeSend: (state, action) => {
-			const sendToRemove = {
+		removeword: (state, action) => {
+			const wordToRemove = {
 				...action.payload,
 			}
-			return state.sendsArray.filter((climb) => climb._id !== sendToRemove._id)
+			return state.wordsArray.filter((climb) => climb._id !== wordToRemove._id)
 		},
 	},
 	extraReducers: (builder) => {
 		builder
-			.addCase(fetchSends.pending, (state) => {
+			.addCase(fetchWords.pending, (state) => {
 				state.isLoading = true
 			})
-			.addCase(fetchSends.fulfilled, (state, action) => {
+			.addCase(fetchWords.fulfilled, (state, action) => {
 				state.isLoading = false
 				state.errMess = null
-				state.sendsArray = action.payload
+				state.wordsArray = action.payload
 			})
-			.addCase(fetchSends.rejected, (state, action) => {
+			.addCase(fetchWords.rejected, (state, action) => {
 				state.isLoading = false
 				state.errMess = action.error ? action.error.message : 'Fetch failed'
-			})
-			.addCase(postSend.pending, (state) => {
-				state.isLoading = true
-			})
-			.addCase(postSend.fulfilled, (state, action) => {
-				state.isLoading = false
-				state.errMess = null
-				state.sendsArray.push(action.payload)
-			})
-			.addCase(postSend.rejected, (state, action) => {
-				state.isLoading = false
-				state.errMess = action.error ? action.error.message : 'Post failed'
-			})
-			.addCase(deleteSend.pending, (state) => {
-				state.isLoading = true
-			})
-			.addCase(deleteSend.fulfilled, (state, action) => {
-				state.isLoading = false
-				state.errMess = null
-				console.log('sendsArray: ', state.sendsArray)
-				console.log('PAYLOAD: ', action.payload)
-				state.sendsArray = state.sendsArray.filter(
-					(climb) => climb._id.toString() !== action.payload
-				)
-			})
-			.addCase(deleteSend.rejected, (state, action) => {
-				state.isLoading = false
-				state.errMess = action.error ? action.error.message : 'Delete failed'
 			})
 	},
 })
 
-export const { logSend, removeSend } = sendsSlice.actions
-export const sendsReducer = sendsSlice.reducer
+export const { logWord, removeWord } = wordsSlice.actions
+export const wordsReducer = wordsSlice.reducer
