6e148728c94f423f022a0295214939956f0b615a
Henry Hartzler <hartzler.henry@gmail.com>
2023-01-08 15:18:49 -0700

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import SimpleContainer from './components/SimpleContainer'
+import HomePage from './pages/HomePage'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
 		<div className='App'>
-			<SimpleContainer />
+			<HomePage />
 		</div>
 	)
 }
