f169516bd846d15472b7a005ce6fbd1ada234166
Henry Hartzler <hartzler.henry@gmail.com>
2023-01-08 15:08:24 -0700

    simple input box and send button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import SimpleContainer from './components/SimpleContainer'
 
 function App() {
 	const [count, setCount] = useState(0)
 
-	return <div className='App'>Hello World</div>
+	return (
+		<div className='App'>
+			<SimpleContainer />
+		</div>
+	)
 }
 
 export default App
