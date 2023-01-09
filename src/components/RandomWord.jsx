9778c667197c793e76e2a798edc6d2e73083132e
Henry Hartzler <hartzler.henry@gmail.com>
2023-01-08 18:05:29 -0700

    seperated words into boxes

--- src/components/RandomWord.jsx
+++ src/components/RandomWord.jsx
@@ -1,11 +1,28 @@
 import { useSelector } from 'react-redux'
+import { Card, CardContent, Container, Typography } from '@material-ui/core'
 
 const RandomWord = () => {
 	const wordsArray = useSelector((state) => state.words.wordsArray)
 	const randomIndex = Math.floor(Math.random() * wordsArray.length)
 
 	if (wordsArray.length > 0) {
-		return <h1>{wordsArray[randomIndex].word}</h1>
+		let selectedWord = wordsArray[randomIndex].word
+		return (
+			<Container style={{ display: 'flex', flexDirection: 'row', padding: 20 }}>
+				{selectedWord.split('').map((el) => (
+					<Card style={{ width: 50, height: 50, margin: 10 }}>
+						<CardContent>
+							<Typography
+								variant='h5'
+								component='h2'
+							>
+								{el}
+							</Typography>
+						</CardContent>
+					</Card>
+				))}
+			</Container>
+		)
 	} else {
 		return <h1>Not enough search results. Please try again!</h1>
 	}
